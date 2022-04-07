2bd93f0cb8f6cc9dde00bcf1a49c6c7b75e40800
Alexander-col <aexro2221@gmail.com>
2022-04-06 20:55:45 -0400

    intial commit to begin the lab!

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "intial push to start lab" << std::endl;
+    std::cout << "Looking forward to start this time lab!" << std::endl;
+
+    return 0;
+}
